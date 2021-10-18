2d2d24151747c0dde6db2cd1be17dbc2600d77ea
Andrew Mackay <andrewmackayd@gmail.com>
2021-10-18 13:03:03 +0100

    3rd commit main

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
+
 const teas = [
   { name: "Early Grey", brand: "Twinings" },
   { name: "Irish Breakfast", brand: "Barry's Tea" },
@@ -11,6 +12,11 @@ const teas = [
   { name: "Rooibos", brand: "Tick Tock" },
   { name: "Green", brand: "Clipper" }
 ];
+
+
+app.get('/api/teas', (req, res) => {
+  res.json(teas)
+})
 
 
 app.listen(5000, function () {
